672892d9df08577ac6af9d40ca33e897b9192f53
Livio <livio.brunner.lb1@gmail.com>
2020-03-14 02:01:47 +0100

    replace datepicker with vuetify slider

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from "vue";
 import VTooltip from "v-tooltip";
 import App from "./App.vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+import vuetify from "./plugins/vuetify";
+
+Vue.use(Vuetify);
 
 Vue.config.productionTip = false;
 Vue.use(VTooltip);
 
 new Vue({
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
